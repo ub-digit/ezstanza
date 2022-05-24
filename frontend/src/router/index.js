ae223bcec2b0de234da8cf5de45ef9c3a3e22f38
David Gustafsson <david.gustafsson@ub.gu.se>
2022-05-24 16:12:06 +0200

    Remove vue default app stuff

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,24 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-
-function loadView(view) {
-    return () => import(`../views/${view}.vue`);
-}
+import Login from '../views/Login.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'login',
+      component: Login
     }, {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => loadView('AboutView')
+      path: '/login',
+      name: 'login',
+      component: Login,
+      meta: { auth: false }
     }
   ]
 })
