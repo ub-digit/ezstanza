2e2af5b863ce3692f5447da2236d56f5f7e09eee
David Gustafsson <david.gustafsson@ub.gu.se>
2022-06-20 14:22:00 +0200

    Use UserView in UserRegistrationView

--- backend/lib/ezstanza_web/views/user_registration_view.ex
+++ backend/lib/ezstanza_web/views/user_registration_view.ex
@@ -1,20 +1,11 @@
 defmodule EzstanzaWeb.UserRegistrationView do
 
   use EzstanzaWeb, :view
-  alias EzstanzaWeb.UserRegistrationView
+  #alias EzstanzaWeb.UserRegistrationView
+  alias EzstanzaWeb.UserView
 
   def render("show.json", %{user: user}) do
-    %{data: render_one(user, UserRegistrationView, "user.json", as: :user)}
+    %{data: render_one(user, UserView, "user.json", as: :user)}
   end
 
-  def render("user.json", %{user: user}) do
-    %{
-      id: user.id,
-      name: user.name,
-      username: user.username,
-      email: user.email,
-      source: user.source,
-      source_id: user.source_id
-    }
-  end
-end
+ end
